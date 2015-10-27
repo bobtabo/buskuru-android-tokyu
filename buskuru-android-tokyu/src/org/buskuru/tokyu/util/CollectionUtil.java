70bd695ec8112c5bffe442cbd655dc42002546fb
聡史 <@NAGASHIBA2>
2015-10-27 19:42:29 +0900

    テストコミット＆プッシュ

--- buskuru-android-tokyu/src/org/buskuru/tokyu/util/CollectionUtil.java
+++ buskuru-android-tokyu/src/org/buskuru/tokyu/util/CollectionUtil.java
@@ -1,6 +1,6 @@
 /*
  * BusKuru is a Busnavi program developed by BobTabo.
- * 
+ *
  * Copyright (c) 2011 BobTabo. All Rights Reserved.
  */
 package org.buskuru.tokyu.util;
@@ -11,17 +11,15 @@ import java.util.Collection;
 
 /**
  * コレクションに対する共通処理を行います。
- * 
+ *
  * @author <a href="mailto:nagashiba@adv-co.com">Satoshi Nagashiba</a>
  * @version $Revision: 187 $ $Date: 2014-05-27 00:58:55 +0900 (火, 27 5 2014) $
  */
 public final class CollectionUtil {
 
-	public static final String EMPTY = "";
-
 	/**
 	 * コレクションが空であるか確認します。
-	 * 
+	 *
 	 * @param collection
 	 *            コレクション
 	 * @return 空の場合 true を返します
@@ -33,7 +31,7 @@ public final class CollectionUtil {
 
 	/**
 	 * コレクションが空ではないか確認します。
-	 * 
+	 *
 	 * @param collection
 	 *            コレクション
 	 * @return 空ではない場合 true を返します
